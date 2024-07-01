db1c0be05671a0f06cf8a38c4fb5c3fe703956f3
Richard-Tolas1 <richardijitola@gmail.com>
2024-07-01 16:48:10 +0100

    Refactor

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,17 +1,48 @@
-import HomeProfile from "../../components/HomeProfile/HomeProfile";
+import { useState } from "react";
+import FeedIdentity, { feedIdentity } from "../../components/FeedIdentity/FeedIdentity";
+import PeopleList from "../../components/Pages/Home/PeopleComponent";
 
 function Home() {
+  const [feedId, setFeedId] = useState<feedIdentity[]>([
+    {
+      imageUrl: "https://via.placeholder.com/40",
+      name: "Chidinma Elekwachi",
+      pronouns: "She/Her",
+      role: "Software Engineer & Technical Writer",
+      description: "I've crafted 100's of copies and scripts for brands",
+      profileViewers: 14,
+      postImpressions: 37,
+      savedItems: 6,
+    },
+  ]);
   return (
-    <div className="flex mx-40 my-5 justify-between">
-      <div>
-        <HomeProfile />
+    <div className="mx-40 my-5">
+      <div className="md:flex gap-20">
+        {/* First section- 1/3: feedIdentity */}
+        <div className="md:w-1/5 hidden md:block">
+          {feedId.map((f, i) => (
+            <div key={i}>
+              <FeedIdentity
+                imageUrl={f.imageUrl}
+                name={f.name}
+                pronouns={f.pronouns}
+                role={f.role}
+                description={f.description}
+                profileViewers={f.profileViewers}
+                postImpressions={f.postImpressions}
+                savedItems={f.savedItems}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="w-full md:w-2/5">
+          {/* This is the feed they share */}
+          <div className=" ">
+            <PeopleList />
+          </div>
+        </div>
       </div>
-      {/* <div>
-        <HomeProfile />
-      </div>
-      <div>
-        <HomeProfile />
-      </div> */}
     </div>
   );
 }
--- src/components/FeedIdentity/FeedIdentity.tsx
+++ src/components/FeedIdentity/FeedIdentity.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import BookMarkIcon from "../../assets/BookMarkIcon";
+import TurnedInIcon from '@mui/icons-material/TurnedIn';
 
 export interface feedIdentity {
   imageUrl: string;
@@ -22,39 +23,78 @@ function FeedIdentity({
   savedItems,
 }: feedIdentity) {
   return (
-    <div className="bg-white rounded-lg py-6 shadow-md mb-4">
-      <div className="my-4 px-6">
-        <div className="flex flex-col text-center">
+    <div>
+      <div className="border rounded-[0.8rem] bg-white">
+        <div>
           <div>
             <img
-              className="h-10 w-10 rounded-full my-4 relative left-16"
+              className="h-10 w-10 rounded-full my-4 relative left-24"
               src={imageUrl}
               alt={name}
             />
-            <h2 className="text-base font-semibold">{name}</h2>
-            <p className="text-xs text-gray-500">
-              {role} • {description}
-            </p>
+          </div>
+          <div className="font-semibold leading-[1.5] text-center">
+            Welcome, {name} !
+          </div>
+          <div className="text-center text-[#0a66c2] hover:underline cursor-pointer text-xs mt-1">
+            Add a Photo
+          </div>
+        </div>
+        <div className="mt-3 border-y hover:bg-[#0000001a]">
+          <div className="my-3 mx-2 text-sm">
+            <div className="flex items-center justify-between">
+              <div>Profile viewers </div>
+              <div className="text-customGreen px-2">{profileViewers}</div>
+            </div>
+            <div className="flex items-center justify-between mt-2">
+              <div>Post impressions</div>
+              <div className="text-customGreen px-2">{postImpressions}</div>
+            </div>
+          </div>
+        </div>
+        <div className="hover:bg-[#0000001a] rounded-b-[0.75rem] py-3">
+          <div className="flex items-center gap-2 mx-2">
+            <div>
+              <TurnedInIcon color="action" />
+            </div>
+            <div className="text-xs font-semibold">Saved items</div>
           </div>
         </div>
       </div>
-      {/* <hr /> */}
-      <div className="border-y px-6 py-2">
-        <p className="mt-2 text-sm font-semibold text-gray-500">
-          Profile viewers{" "}
-          <span className="text-customGreen px-2">{profileViewers}</span>
-        </p>
-        <p className="mt-2 text-gray-500 font-semibold text-sm">
-          Post impressions{" "}
-          <span className="text-customGreen px-2">{postImpressions}</span>
-        </p>
-      </div>
-      {/* <hr className=" " /> */}
-      <div className="mt-4 text-gray-700 px-6">
-        Saved Items{" "}
-        <span className="text-customGreen px-2">{savedItems}</span>
-      </div>
     </div>
+    // <div className="bg-white rounded-lg py-6 shadow-md mb-4">
+    //   <div className="my-4 px-6">
+    //     <div className="flex flex-col text-center">
+    //       <div>
+    //         <img
+    //           className="h-10 w-10 rounded-full my-4 relative left-16"
+    //           src={imageUrl}
+    //           alt={name}
+    //         />
+    //         <h2 className="text-base font-semibold">{name}</h2>
+    //         <p className="text-xs text-gray-500">
+    //           {role} • {description}
+    //         </p>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {/* <hr /> */}
+    //   <div className="border-y px-6 py-2">
+    //     <p className="mt-2 text-sm font-semibold text-gray-500">
+    //   Profile viewers{" "}
+    //   <span className="text-customGreen px-2">{profileViewers}</span>
+    //     </p>
+    //     <p className="mt-2 text-gray-500 font-semibold text-sm">
+    //       Post impressions{" "}
+    //       <span className="text-customGreen px-2">{postImpressions}</span>
+    //     </p>
+    //   </div>
+    //   {/* <hr className=" " /> */}
+    //   <div className="mt-4 text-gray-700 px-6">
+    //     Saved Items{" "}
+    //     <span className="text-customGreen px-2">{savedItems}</span>
+    //   </div>
+    // </div>
   );
 }
 
